0287216aad329e1d1450d2726941e5550d1223b8
vitia <vitia.iezhovqqqq@gmail.com>
2022-01-02 17:21:41 +0700

    Redesigned folders. Add static class TextParser

---
+++ Markov Chain Text Generator/Markov/TextParser.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Markov_Chain_Text_Generator.Markov
+{
+	internal static class TextParser
+	{
+
+	}
+}
