1197139846ade6755fcd84e2ef684b4cd27d4c3f
vitia <vitia.iezhovqqqq@gmail.com>
2022-01-03 13:42:49 +0700

    replace TexParser on TextGenerator. Change namespace of End and Start Nodes

--- Markov Chain Text Generator/Markov/Nodes and connections/SpecialNodes/TextStartMarkovNode.cs
+++ Markov Chain Text Generator/Markov/Nodes and connections/SpecialNodes/TextStartMarkovNode.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Markov_Chain_Text_Generator.Markov.SpecialNodes
+namespace Markov_Chain_Text_Generator
 {
     internal class TextStartMarkovNode : INode<string>
     {
---
+++ Markov Chain Text Generator/Markov/TextGenerator.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Markov_Chain_Text_Generator.Markov
+{
+	internal class TextGenerator
+	{
+		private void Push(string word)
+        {
+            throw new NotImplementedException();
+        }
+
+		public TextGenerator (StreamReader streamReader)
+        {
+            throw new NotImplementedException ();
+        }
+
+		public TextGenerator (string text)
+        {
+            throw new NotImplementedException ();
+        }
+
+        private TextStartMarkovNode _start;
+        private TextMarkovNode _lastReadNode;
+    }
+}
