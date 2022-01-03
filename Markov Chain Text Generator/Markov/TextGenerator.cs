9a6387fce169c8e2d670c84c3308d410be892172
vitia <vitia.iezhovqqqq@gmail.com>
2022-01-03 14:36:43 +0700

    make method to clear the line from extra spaces

--- Markov Chain Text Generator/Markov/TextGenerator.cs
+++ Markov Chain Text Generator/Markov/TextGenerator.cs
@@ -2,28 +2,37 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 
 namespace Markov_Chain_Text_Generator.Markov
 {
 	internal class TextGenerator
 	{
+		private static readonly Regex _extraSpacesRegex = new Regex(@"\s+", RegexOptions.Compiled);
+		private static readonly string _extraSpacesReplaceTarget = " ";
+
 		private void Push(string word)
-        {
-            throw new NotImplementedException();
-        }
+		{
+			throw new NotImplementedException();
+		}
+
+		private string RemoveExtraSpaces (string str)
+		{
+			return _extraSpacesRegex.Replace(str, _extraSpacesReplaceTarget);
+		}
 
 		public TextGenerator (StreamReader streamReader)
-        {
-            throw new NotImplementedException ();
-        }
+		{
+			throw new NotImplementedException ();
+		}
 
 		public TextGenerator (string text)
-        {
-            throw new NotImplementedException ();
-        }
+		{
+			throw new NotImplementedException ();
+		}
 
-        private TextStartMarkovNode _start;
-        private TextMarkovNode _lastReadNode;
-    }
+		private TextStartMarkovNode _start;
+		private TextMarkovNode _lastReadNode;
+	}
 }
--- Markov Chain Text Generator/Program.cs
+++ Markov Chain Text Generator/Program.cs
@@ -1,38 +1,68 @@
 ﻿using System.Text.Json;
 using Markov_Chain_Text_Generator;
 
-
-var fish = new TextMarkovNode("fish");
-var one = new TextMarkovNode("one");
-var two = new TextMarkovNode("two");
-var three = new TextMarkovNode("three");
-
-
-fish.AddTransition(one);
-fish.AddTransition(two);
-fish.AddTransition(three);
-fish.AddTransition(fish);
-
-
-Console.WriteLine(fish);
-
-var dict = new Dictionary<string, int>
+namespace Markov_Chain_Text_Generator
 {
-	["one"]= 0,
-	["two"]= 0,
-	["three"]= 0,
-	["fish"]= 0,
-};
+	class Programm
+	{
+		public static void Main()
+		{
+			var fish = new TextMarkovNode("fish");
+			var one = new TextMarkovNode("one");
+			var two = new TextMarkovNode("two");
+			var three = new TextMarkovNode("three");
 
-for (int i = 0; i < 1000; i++)
-{
-	dict[fish.NextNode.State]++;
+
+			fish.AddTransition(one);
+			fish.AddTransition(two);
+			fish.AddTransition(three);
+			fish.AddTransition(fish);
+
+
+			Console.WriteLine(fish);
+
+			var dict = new Dictionary<string, int>
+			{
+				["one"] = 0,
+				["two"] = 0,
+				["three"] = 0,
+				["fish"] = 0,
+			};
+
+			for (int i = 0; i < 1000; i++)
+			{
+				dict[fish.NextNode.State]++;
+			}
+
+			Console.WriteLine(
+					JsonSerializer.Serialize<Dictionary<string, int>>(
+						dict,
+						new JsonSerializerOptions { WriteIndented = true }
+					)
+				);
+
+			using (StreamReader streamReader = new StreamReader("test.txt"))
+			{
+				while (!streamReader.EndOfStream)
+					foreach (string word in streamReader.ReadLine().Split(" "))
+						if (!isSpaces(word))
+							Console.WriteLine("word:" + word);
+			}
+		}
+
+		public static bool isSpaces(string str)
+		{
+			foreach (char sym in str)
+            {
+				if (sym != ' ') return false;
+			}
+			return true;
+		}
+	}
 }
 
-Console.WriteLine(
-		JsonSerializer.Serialize<Dictionary<string, int>>(
-			dict, 
-			new JsonSerializerOptions { WriteIndented = true }
-		)
-	);
+
+
+
+
 
